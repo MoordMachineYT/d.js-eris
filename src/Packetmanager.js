df25522b9b7befabee73df42a6f991f5f69f6b72
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-05-10 17:07:57 +0100

    Update Packetmanager.js

--- src/Packetmanager.js
+++ src/Packetmanager.js
@@ -1,3 +1,23 @@
 class Packetmanager {
-  
+  constructor(client) {
+    this.client = client;
+    this.eris = this.client.eris;
+  }
+  packet(packet, shard) {
+    if (!packet) throw new Error("received an empty packet");
+    if (packet.op !== 0) return;
+    if (!this.client.options.disabledEvents[packet.t]) {
+      packet.shard = shard;
+      this.handle(packet);
+    }
+  }
+  handle(packet) {
+    switch(packet.t) {
+      case "MESSAGE_CREATE": {
+        
+      }
+    }
+  }
 }
+
+module.exports = Packetmanager;
