c1f0d43498e640538b834e6881eaee49c2a93a43
MoordMachineYT <33372542+MoordMachineYT@users.noreply.github.com>
2018-05-10 16:31:23 +0100

    Update Client.js

--- src/Client.js
+++ src/Client.js
@@ -1,1 +1,37 @@
+var EventEmitter = require("events").EventEmitter;
+const Util = require("./Util/Util.js");
+try {
+  EventEmitter = require("eventemitter16");
+} catch(err) {}
+const eris = require("eris");
 
+class Client extends EventEmitter{
+  constructor(options) {
+    super();
+    this.options = {
+      shardId: 0,
+      shardCount: 0,
+      messageCacheMaxSize: 200,
+      disabledEvents: [],
+      fetchAllMembers: false,
+      ws: null,
+      disableEveryone: true,
+      compress: true,
+      autoreconnect: true,
+      largeThreshold: 250,
+      latencyThreshold: 4000,
+      guildCreateTimeout: 2000,
+      connectionTimeout: 30000
+    };
+    if (typeof options === "object") {
+      for (var i of Object.keys(options)) this.options[i] = options[i];
+    }
+  }
+  login(token) {
+    if (this.eris && this.eris.ready) throw new Error("Already logged in");
+    if (!token) Promise.reject(new Error("No token given"));
+    if (!this.eris) {
+      
+    }
+  }
+}
